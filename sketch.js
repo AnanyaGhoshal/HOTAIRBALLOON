067451f7b3b9b2950fab6fed298261af0b19c3ba
AnanyaGhoshal <76252188+AnanyaGhoshal@users.noreply.github.com>
2021-03-24 09:46:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,80 @@
+var hballoon; 
+var database, position;
+var cityImg, airy;
+
+function preload(){
+
+  cityImg = loadImage("cityImage.png");
+  airy = loadAnimation("HotAirBallon-01.png","HotAirBallon-02.png","HotAirBallon-03.png");
+
+}
+
+function setup() {
+
+  database = firebase.database();
+
+  createCanvas(900,500);
+  hballoon = createSprite(150, 350, 10, 10);
+  hballoon.addAnimation("flying",airy);
+  hballoon.scale = 0.5;
+  var hballoonPosition = database.ref('Balloon/Position');
+  hballoonPosition.on("value",readPosition,showError);
+
+}
+
+function draw() {
+  background(cityImg);  
+
+  if(keyDown(UP_ARROW)){
+
+    writePosition(0,-5);
+    hballoon.addAnimation("flying",airy);
+    hballoon.scale = hballoon.scale-0.01;
+
+  }
+  else if(keyDown(DOWN_ARROW)){
+
+    writePosition(0,+5);
+    hballoon.addAnimation("flying",airy);
+    hballoon.scale = hballoon.scale+0.01;
+
+
+  }
+  else if(keyDown(LEFT_ARROW)){
+
+    writePosition(-5,0);
+
+  }
+  else if(keyDown(RIGHT_ARROW)){
+
+    writePosition(5,0);
+
+  }
+
+
+  drawSprites();
+}
+
+function writePosition(x,y){
+
+  database.ref('Balloon/Position').set({
+
+    'x': position.x+x,
+    'y': position.y+y
+
+  })
+}
+
+function readPosition(data){
+
+  position = data.val();
+  hballoon.x = position.x;
+  hballoon.y = position.y;
+
+}
+
+function showError(){
+
+  console.log("There is an error to write in database");
+
+}
